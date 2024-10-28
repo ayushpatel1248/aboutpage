d9efffdeb43929fe24e4d58f3843ac97b8a1e9a5
ayush <ayushpatel1248@gmail.com>
2024-10-28 15:51:07 +0530

    s

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -19,8 +19,11 @@ export async function POST(req: Request) {
     }
 
     return new Response(JSON.stringify(data), { status: 200 });
-  } catch (error) {
-
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-  }
+  }  catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+    return new Response(
+      JSON.stringify({ error: errorMessage }),
+      { status: 500 }
+    );
+}
 }
